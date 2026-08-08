6d5675b61d5e9469369da432625e1a66748a3f87
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2026-08-08 14:21:40 -0400

    add calendar grid

--- src/calendar/Calendar.tcl
+++ src/calendar/Calendar.tcl
@@ -14,7 +14,20 @@ snit::widget Calendar {
         set date [clock scan $date_str -format "%d/%m/%Y %H:%M:%S"]
 
         set paginator [CalendarPaginator $win.paginator -date [myvar date] -selected_date [myvar selected_date]]
+        set grid [CalendarGrid $win.grid -date [myvar date] -selected_date [myvar selected_date]]
+
 
         pack $paginator -fill x -padx 4 -pady 4
+        pack $grid -fill both -expand yes -padx 4 -pady 4
+
+        trace add variable selected_date write [mymethod date_selected]
+    }
+
+    destructor {
+        trace remove variable selected_date write [mymethod date_selected]
+    }
+
+    method date_selected {args} {
+        puts [clock format $selected_date -format %D]
     }
 }
