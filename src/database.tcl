c1d776b8742f902a0211ef9a952799975c0c2c74
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2026-08-10 20:11:39 -0400

    add database migration

---
+++ src/database.tcl
@@ -0,0 +1,38 @@
+db eval {PRAGMA foreign_keys = ON}
+
+db eval {
+    CREATE TABLE IF NOT EXISTS migrations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        version INTEGER
+    ) STRICT
+}
+
+set version [db eval {SELECT version FROM migrations LIMIT 1}]
+if {$version == ""} {
+    set version 0
+    db eval {INSERT INTO migrations (version) VALUES (:version)}
+}
+
+if {$version == 0} {
+    db transaction {
+        db eval {
+            CREATE TABLE IF NOT EXISTS workers (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT UNIQUE NOT NULL,
+                weight INTEGER NOT NULL
+            ) STRICT
+        }
+
+        db eval {
+            CREATE TABLE IF NOT EXISTS calendar (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                worker_id INTEGER NOT NULL REFERENCES workers (id),
+                date TEXT UNIQUE NOT NULL
+            ) STRICT
+        }
+
+        incr version
+
+        db eval {UPDATE migrations SET version = :version}
+    }
+}
